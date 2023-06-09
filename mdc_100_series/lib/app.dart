ee984a4ccd5225bb0c5b5acb1b54fa0192186433
gcruz <Gersoncruz301115@gmail.com>
2023-06-09 09:45:05 -0600

    add
    Botones, controladores y navigation

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -24,7 +24,7 @@ class ShrineApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shrine',
+      title: 'Codelabs6',
       initialRoute: '/login',
       routes: {
         '/login': (BuildContext context) => const LoginPage(),
